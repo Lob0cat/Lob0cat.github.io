89d8f020144e306d1a97539ff1a09ba89ed41d54
mehmet güner <161369771+Lob0cat@users.noreply.github.com>
2026-01-04 23:53:53 +0300

    Delete js directory

--- js/router.js
+++
@@ -1,8 +0,0 @@
-export function getRoute() {
-  return location.hash.replace("#", "");
-}
-
-export function onRouteChange(cb) {
-  window.addEventListener("hashchange", cb);
-  window.addEventListener("load", cb);
-}
